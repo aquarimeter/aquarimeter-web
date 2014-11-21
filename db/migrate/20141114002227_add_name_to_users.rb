7cd8116b4e253db500644066b8cfe1c275192562
Robert O'Connor <rob@oconnor.ninja>
2014-11-21 15:54:29 -0500

    Tweak the name migration to add first and last name as separate fields.

--- db/migrate/20141114002227_add_name_to_users.rb
+++ db/migrate/20141114002227_add_name_to_users.rb
@@ -1,5 +1,7 @@
 class AddNameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :name, :string
+    add_column :users, :first_name, :string
+    add_column :users, :surname, :string
+
   end
 end
