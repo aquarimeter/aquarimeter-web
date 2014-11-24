5f4ac6e9d38bb8d8d0018595b02ca400050b57ef
Robert O'Connor <rob@oconnor.ninja>
2014-11-24 04:28:53 -0500

    beef up the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,16 @@
 describe User do
 
-  before(:each) { @user = User.new(email: 'user@example.com') }
+  before(:each) { @user = User.new(email: 'user@example.com', first_name:"A", last_name:"User") }
 
   subject { @user }
 
   it { should respond_to(:email) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:last_name) }
 
   it "#email returns a string" do
     expect(@user.email).to match 'user@example.com'
+    expect(@user.first_name).to match 'A'
+    expect(@user.last_name).to match 'User'
   end
-
 end
