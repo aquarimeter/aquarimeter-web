0cc4e13184f69cf064d868f173ed1d1bebb4539c
Robert O'Connor <rob@oconnor.ninja>
2014-11-26 16:25:13 -0500

    tweak...

--- db/migrate/20141114002221_devise_create_users.rb
+++ db/migrate/20141114002221_devise_create_users.rb
@@ -2,8 +2,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -30,8 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :unlock_token # Only if unlock strategy is :email or :both
       t.datetime :locked_at
 
-      t.string :first_name, :limit => 255, :null => false, :default => ""
-      t.string  :last_name, :limit => 255, :null => false, :default => ""
+      t.string :first_name, :limit => 255, :null => false
+      t.string  :last_name, :limit => 255, :null => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20141119001542) do
   add_index "sensor_readings", ["aquariums_id"], name: "index_sensor_readings_on_aquariums_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              null: false
+    t.string   "encrypted_password",                 null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -59,11 +59,11 @@ ActiveRecord::Schema.define(version: 20141119001542) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
+    t.integer  "failed_attempts",        default: 0, null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "first_name",             default: "", null: false
-    t.string   "last_name",              default: "", null: false
+    t.string   "first_name",                         null: false
+    t.string   "last_name",                          null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role"
