45d6d1d36ce5cbb4ce8670532672ea7b43845d58
Robert O'Connor <rob@oconnor.ninja>
2014-11-28 03:45:59 -0500

    Random changes...

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :user do
     confirmed_at Time.now
-    name "Test User"
+    first_name "Test"
+    last_name "User"
     email "test@example.com"
     password "please123"
 
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,7 +1,9 @@
 module Features
   module SessionHelpers
-    def sign_up_with(email, password, confirmation)
+    def sign_up_with(first_name, last_name,email, password, confirmation)
       visit new_user_registration_path
+      fill_in 'First name', :with => first_name
+      fill_in 'Last name', :with => last_name
       fill_in 'Email', with: email
       fill_in 'Password', with: password
       fill_in 'Password confirmation', :with => confirmation
