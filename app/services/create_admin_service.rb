e9cd554a14c8a4d3e08a7fcd59a70528fc14acf5
Robert O'Connor <rob@oconnor.ninja>
2014-12-02 21:29:38 -0500

    Update secrets.yml to pull from the figaro...NEVER EVER EVER EVER store sensitive information such as passwords in git!

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,8 @@
 class CreateAdminService
   def call
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.first_name = "Admin"
-        user.last_name = "User"
+        user.first_name = Rails.application.secrets.admin_first_name
+        user.last_name = Rails.application.secrets.admin_last_name
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.confirm!
