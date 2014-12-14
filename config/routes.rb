1b3bab59076e9a706b9405c69142d8cd39593f24
Robert O'Connor <rob@oconnor.ninja>
2014-12-14 07:16:05 -0500

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :path_prefix => 'api/v1', :skip => [:sessions, :registrations, :passwords]
+  devise_for :users, :path_prefix => 'api/v1', :skip => [:sessions, :registrations, :password,:confirmation, :unlock]
 
   namespace :api do
     namespace :v1, :format => :json do
@@ -10,12 +10,11 @@ Rails.application.routes.draw do
       resources :aquariums,defaults: { format: 'json' } do
         resource :readings, only: [:index, :create, :show, :new], defaults: { format: 'json' }
       end
-
     end
-    mount MailPreview => 'mail_view' if Rails.env.development?
-    root to: 'static#index'
-    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   end
-
+  mount MailPreview => 'mail_view' if Rails.env.development?
+  root to: 'static#index'
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users, :only => [:password,:confirmation,:unlock]
 
 end
