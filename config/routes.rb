b0a433dcdc2589b9e5b5cf72c01f542afbc968e4
Robert O'Connor <rob@oconnor.ninja>
2014-12-22 07:56:01 -0500

    All user registration, edit, sign up, confirmation and password reset pages are styled :D Thanks Mark <3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, :path_prefix => 'api/v1', :ip => [:sessions, :registrations, :password,:confirmation, :unlock]
 
   namespace :api do
     namespace :v1, :format => :json do
@@ -7,15 +6,20 @@ Rails.application.routes.draw do
         post 'login' => 'sessions#create', :as => :login, defaults: { format: 'json' }
         delete 'logout' => 'sessions#destroy', :as => :logout, defaults: { format: 'json' }
         post 'register' => 'registrations#create', :as => :register
+        patch 'update_user' => 'registrations#updates', :as => :update_info
         delete 'delete_account' => 'registrations#destroy', :as => :delete_account
       end
       resources :aquariums,param: :name,defaults: { format: 'json' } do
-        resource :readings, only: [:index, :create, :show, :new], defaults: { format: 'json' }
+        resource :readings
+        resources :images
       end
     end
   end
+  devise_for :user
+  resources :aquariums, only: [:show, :update, :destroy],param: :name
+
   mount MailPreview => 'mail_view' if Rails.env.development?
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :only => [:password,:confirmation,:unlock]
+  root to: 'aquariums#index'
 
 end
