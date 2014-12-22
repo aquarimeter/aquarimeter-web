98209a38dd82667d8adc6cd321d68c41e57c291b
Robert O'Connor <rob@oconnor.ninja>
2014-12-21 20:29:45 -0500

    Fixed the REST API for posting readings.

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -1,21 +1,28 @@
 class Api::V1::ReadingsController < Api::V1::BaseController
-  before_filter :authenticate_user, :except => :index
+  before_filter :authenticate_user, :except => [:show, :index]
+  before_filter :set_aquarium
 
-  def index
-    @readings = SensorReading.all
-    render @readings, status: :ok
+  def show
+
   end
 
   def create
     @reading = SensorReading.new(reading_params)
+    @reading.aquarium = @aquarium
     if @reading.save
       render :json => @reading, status: :created, location => [:api, @reading]
     else
       render json => @reading.errors, status => :unprocessable_entity
     end
   end
-
+  def update
+    render status: :not_implemented, json: {message: "Cannot update readings"}
+  end
   private
+  def set_aquarium
+    @aquarium = Aquarium.find_by(:name => params[:aquarium_name])
+  end
+
   def reading_params
     params.require(:reading).permit(:ph, :temperature)
   end
