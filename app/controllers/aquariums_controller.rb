fc0458754f372396bc45dc612dd647d6d62a728d
Robert O'Connor <rob@oconnor.ninja>
2014-12-23 09:38:57 -0500

    Make it a tad less error prone when going to an aquarium which does not exist

--- app/controllers/aquariums_controller.rb
+++ app/controllers/aquariums_controller.rb
@@ -1,46 +1,28 @@
 class AquariumsController < ApplicationController
   before_filter :set_aquarium
   before_filter :authenticate_user!, :only => [:show, :index]
-
   respond_to :html
   def show
-    respond_with do |format|
-      format.html { @aquarium }
+    unless @aquarium
+      flash[:error] = "Aquarium #{params[:name]} does not exist."
+      redirect_to root_url
+    else
+      respond_with @aquarium
     end
   end
 
 
   def index
     @aquariums = Aquarium.where(:user_id => current_user.id)
-    respond_with do |format|
-      format.html { @aquariums }
-    end
+    respond_with html: @aquariums
   end
 
+  # update avatar
   def update
-    @aquarium = Aquarium.find_by params[:name]
 
   end
-  def photo
-    tempfile = Tempfile.new("photoupload")
-    tempfile.binmode
-    tempfile << request.body.read
-    tempfile.rewind
 
-    photo_params = params.slice(:filename, :type, :head).merge(:tempfile => tempfile)
-    photo = ActionDispatch::Http::UploadedFile.new(photo_params)
 
-    @postcard = Postcard.find(params[:id])
-    @postcard.photo = photo
-
-    respond_to do |format|
-      if @postcard.save
-        format.json { head :ok }
-      else
-        format.json { render :json => @postcard.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
     protected
 
     def set_aquarium
