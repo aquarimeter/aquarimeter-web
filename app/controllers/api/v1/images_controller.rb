15a89cb7fa795eda43a222d4609b8ae76485e127
Robert O'Connor <rob@oconnor.ninja>
2014-12-23 22:03:30 -0500

    Documentation and such

---
+++ app/controllers/api/v1/images_controller.rb
@@ -0,0 +1,77 @@
+class Api::V1::ImagesController < Api::V1::BaseController
+  before_filter :authenticate_user, :except => [:index,:show]
+  before_filter :set_aquarium
+
+
+  def new
+    @image = Image.new(:aquarium_id => Aq)
+  end
+
+
+  # taken from http://ograycoding.wordpress.com/2012/07/03/api-upload-with-carrierwave/
+  # assumes the following json
+  def create
+    #check if file is within picture_path
+    if params[:image][:image_path]["file"]
+      picture_path_params = params[:image][:image_path]
+      #create a new tempfile named fileupload
+      tempfile = Tempfile.new("fileupload")
+      tempfile.binmode
+      #get the file and decode it with base64 then write it to the tempfile
+      tempfile.write(Base64.decode64(picture_path_params["file"]))
+
+      #create a new uploaded file
+      uploaded_file = ActionDispatch::Http::UploadedFile.new(:tempfile => tempfile, :filename => picture_path_params["filename"], :original_filename => picture_path_params["original_filename"])
+
+      #replace picture_path with the new uploaded file
+      params[:image][:image_path] =  uploaded_file
+
+    end
+
+    @image = Image.new(params[:image])
+
+    respond_to do |format|
+      if @image.save
+        format.html { redirect_to @image, notice: 'Image was successfully created.' }
+        format.json { render json: @image, status: :created, location: @image }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @image.errors, status: :unprocessable_entity }
+      end
+    end
+
+    def edit
+      @image = Image.find(params[:id])
+    end
+
+    def update
+      @image = Image.find(params[:id])
+
+      if @image.update_attributes(params[:image])
+        respond_with do |format|
+          format.html do
+            flash[:notice] = "Successfully updated image."
+            redirect_to @image.aquarium
+          end
+          format.json { render json: @image.aquariam, :status => :created }
+        end
+      else
+        respond_with do |format|
+          format.html { render :action, 'edit' }
+          format.json { render json: @image.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @image = Image.find(params[:id])
+      @image.destroy
+      flash[:notice] = "Successfully destroyed image."
+      redirect_to @image.gallery
+    end
+  end
+  private
+  def set_aquarium
+    @aquarium = Aquarium.find_by :name => params[:aquarium_name]
+  end
+end
---
+++ app/controllers/datatables/sensor_reading_datatable.rb
@@ -0,0 +1,60 @@
+# helper class to serializing to JSON and pagination of readings gracefully.
+# adapted from http://railscasts.com/episodes/340-datatables
+# Most of this is straight-forward --
+class SensorReadingDatatable
+  delegate :params, :h, to: :@view
+
+  def initialize(view)
+    @view = view
+  end
+
+  def as_json(options = {})
+    {
+        sEcho: params[:sEcho].to_i,
+        iTotalRecords: SensorReading.count,
+        iTotalDisplayRecords: readings.total_entries,
+        aaData: data
+    }
+  end
+
+  private
+
+  def data
+    readings.map do |readings|
+      [
+          (readings.created_at.strftime('%b %e %Y %H:%M:%S')),
+          (readings.ph),
+          (readings.temperature)
+      ]
+    end
+  end
+
+  def readings
+    @readings ||= fetch_readings
+  end
+
+  def fetch_readings
+    readings = SensorReading.order("#{sort_column} #{sort_direction}")
+    readings = readings.page(page).per_page(per_page)
+    readings
+  end
+
+
+
+  def page
+    params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+  end
+
+  def sort_column
+    columns = %w[ph temperature created_at]
+    columns[params[:iSortCol_0].to_i]
+  end
+
+  def sort_direction
+    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+  end
+end
