6016b0db4fade06f2eb014f5e2d65f5661f7069c
Robert O'Connor <rob@oconnor.ninja>
2014-12-25 19:59:28 -0500

    remove mysql2 in favaor of postgres

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,40 +1,82 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+require 'mina/bundler'
+require 'mina/rails'
+require 'mina/git'
+require 'mina/rvm'
+require 'mina_sidekiq/tasks'
+require 'mina/unicorn'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :domain, '104.131.90.94'
+set :deploy_to, '/home/deployer/aquarimeter'
+set :repository, 'https://github.com/aquarimeter/aquarimeter-web'
+set :branch, 'master'
+set :user, 'deployer'
+set :forward_agent, true
+set :port, '22'
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+set :rvm_path, "/usr/local/rvm/scripts/rvm"
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'log', 'config/initializers/devise.rb','config/application.yml']
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
+task :environment do
+  queue %{
+echo "-----> Loading environment"
+#{echo_cmd %[source ~/.bashrc]}
+}
+  invoke :'rvm:use[ruby-2.1.5@aquarimeter_web]'  
+end
 
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/shared/log"]
+  queue! %[mkdir -p "#{deploy_to}/shared/config/initializers"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
 
-namespace :deploy do
+  queue! %[mkdir -p "#{deploy_to}/shared/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
+  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
+
+  queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
+  queue %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
+  
+  queue! %[touch "#{deploy_to}/shared/config/application.yml"]
+  queue %[echo "-----> Be sure to edit 'shared/config/application.yml'."]
+
+  queue! %[touch "#{deploy_to}/shared/config/initializers/devise.rb"]
+  # sidekiq needs a place to store its pid file and log file
+  queue! %[mkdir -p "#{deploy_to}/shared/pids/"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/pids"]
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  deploy do
+
+    # stop accepting new workers
+    invoke :'sidekiq:quiet'
+
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
+
+    to :launch do
+      invoke :'sidekiq:restart'
+      invoke :'unicorn:restart'
     end
   end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
 end
