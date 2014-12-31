007d87bf3d3d11817861cdf5f2c9139447afbf7b
Robert O'Connor <rob@oconnor.ninja>
2014-12-31 03:28:29 -0500

    tweak to include database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,9 +45,6 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
-
   queue! %[touch "#{deploy_to}/shared/config/secrets.yml"]
   queue %[echo "-----> Be sure to edit 'shared/config/secrets.yml'."]
 
