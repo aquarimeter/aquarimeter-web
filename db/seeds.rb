c1e5f7b5a5e6b9bd35afecd9259c93b9486f4f0e
Robert O'Connor <rob@oconnor.ninja>
2015-01-04 05:07:58 -0500

    Move the seed rake task to the db seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
 
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+file = File.expand_path('../../readings.csv', __FILE__)
+require 'csv'
+CSV.foreach(file) do |row|
+  temp = row[0].to_f
+  ph = row[1].to_f
+  SensorReading.create!(aquarium_id:1, temperature:temp, ph:ph)
+  # Kernel.sleep(1) # add some latency...this is so I can graph over time and calculate mock avgs
+end
